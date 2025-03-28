63f8b213560d09d1f9375d9c9682fa54c8f9a343
Rizka <uziyraf@gmail.com>
2025-03-28 20:39:45 +0700

    menambah loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,19 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+    ],
+  },
 };
